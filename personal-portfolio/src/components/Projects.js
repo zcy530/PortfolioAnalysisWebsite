fa954c8d37c21af8f094f412af7b66df0fcc565c
Caiyi Zhang <t-caiyizhang`@microsoft.com>
2022-07-21 19:19:28 +0800

    Modify questionnaire part

--- personal-portfolio/src/components/Projects.js
+++ personal-portfolio/src/components/Projects.js
@@ -5,42 +5,66 @@ import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
+import React, { useState } from 'react';
 import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
 
-  const projects = [
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-  ];
+  const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
 
   return (
     <section className="project" id="project">
@@ -66,22 +90,34 @@ export const Projects = () => {
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     <Tab.Pane eventKey="first">
-                     
-			<h2>Risk Tolerance Questionnaire</h2>
-			<p>Tolerance for risk is a key consideration in determining your probable level of comfort with varying investing choices.
-			Your risk tolerance is scored based on three factors: (1) time horizon, (2) long-term goals and expectations, and 
-			(3) short-term risk attitudes.</p>					
 
-			<input id="element_3_1" name="element_3" class="element radio" type="radio" value="1" required/>
-<label class="choice" for="element_3_1">A. Less than 45</label>
-<input id="element_3_2" name="element_3" class="element radio" type="radio" value="2" required/>
-<label class="choice" for="element_3_2">B. 45 to 55</label>
-<input id="element_3_3" name="element_3" class="element radio" type="radio" value="3" required/>
-<label class="choice" for="element_3_3">C. 56 to 65</label>
-<input id="element_3_4" name="element_3" class="element radio" type="radio" value="4" required/>
-<label class="choice" for="element_3_4">D. 66 to 75</label>
-<input id="element_3_5" name="element_3" class="element radio" type="radio" value="5" required/>
-<label class="choice" for="element_3_5">E. Older than 75</label>
+                    <div className='app'>
+                      {showScore ? (
+                        <>
+                        <div className='score-section'>
+                          You scored {score} out of {questions.length}
+                        </div>
+                        
+                        </>
+                      ) : (
+                        <>
+                          <div className='question-section'>
+                            <div className='question-count'>
+                              <span>Question {currentQuestion + 1}</span>/{questions.length}
+                            </div>
+
+                            <div className='question-text'>{questions[currentQuestion].questionText}</div>
+                          </div>
+
+                          <div className='answer-section'>
+                            {questions[currentQuestion].answerOptions.map((answerOption) => (
+                              <button className='qbutton' onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                            ))}
+                          </div>
+                        </>
+                      )}
+                    </div>
+
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
                       <p>Lorem </p>
